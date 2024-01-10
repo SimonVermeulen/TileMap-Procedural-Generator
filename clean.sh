957f34a3c6a4bb25d1e0093e47a5762850b26fe5
SimonVermeulen <simon.vermeulen@epitech.eu>
2024-01-10 23:26:44 +0100

    [Feat](Global): assets/base

---
+++ clean.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -r CMakeFiles
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm Makefile
+
+rm -r ./src/CMakeFiles
+rm -r ./src/cmake_install.cmake
+rm ./src/Makefile
+
+rm -r ./src/Graphic/CMakeFiles
+rm -r ./src/Graphic/cmake_install.cmake
+rm ./src/Graphic/Makefile
+
+rm -r ./src/Utils/CMakeFiles
+rm -r ./src/Utils/cmake_install.cmake
+rm ./src/Utils/Makefile
+
+rm -r ./src/WaveFunctionCollapse/CMakeFiles
+rm -r ./src/WaveFunctionCollapse/cmake_install.cmake
+rm ./src/WaveFunctionCollapse/Makefile
